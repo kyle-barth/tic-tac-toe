20388f6de6bef27bb86587e0e14dde528753f4ce
kyle-barth <kylebarth@me.com>
2019-04-05 16:57:58 +0100

    v1.0 of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
-class App extends Component {
-  render() {
+export default function App() {
+  const initBoardState: string[] = new Array(9).fill('');
+  const initPlayerState: boolean = true;
+  
+  let [boardState, setBoardState] = useState(initBoardState);
+  let [playerTurnState, setTurnState] = useState(initPlayerState);
+
+  function reset() {
+    setBoardState(initBoardState);
+    setTurnState(true);
+  }
+
+  function addMove(index: number) {    
+    if (boardState[index] === ''){
+      setTurnState(!playerTurnState);
+
+      if (playerTurnState) {
+        setBoardState([...boardState.slice(0, index), 'X', ...boardState.slice(index+1)]);
+      } else {
+        setBoardState([...boardState.slice(0, index), 'O', ...boardState.slice(index+1)]);
+      }
+    }
+  }
+
+  function renderBoard(boardState: string[]) {
+    const renderTiles = boardState.map((x, index) => 
+        <div key={ index } className='tile' onClick={() => { addMove(index) } }>
+          { x }
+        </div>
+      )
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='board'>
+        { renderTiles }
       </div>
     );
   }
-}
 
-export default App;
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>tic-tac-toe</h1>
+
+        { renderBoard(boardState) }
+
+        <Button variant="contained" style={{marginTop: "50px"}} onClick={() => { reset() }}>
+          Reset Game
+        </Button>
+      </header>
+    </div>
+  );
+}
